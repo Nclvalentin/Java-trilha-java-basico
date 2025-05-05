d0671e947499750873132081774136e2be23f8cf
Nathan Costa <Nclvalentin02@gmail.com>
2025-05-04 23:13:36 -0300

    inclusão Tipos e Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Nathan Costa Louro Valentin";
+        System.out.println(meuNome);
+
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.err.println(numeroCurto);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto2);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.printf("O VALOR DE PI É " ,VALOR_DE_PI);
     }
 }
