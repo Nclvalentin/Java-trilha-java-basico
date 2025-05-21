3b30ebd71c0e505c788f8d314a07c150deaa52c6
Nathan Costa <Nclvalentin02@gmail.com>
2025-05-21 01:47:21 -0300

    Exceção

---
+++ Tratamento-De-Excecoes/src/FormatadorCepExemplo.java
@@ -0,0 +1,22 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try{
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+
+        }catch(CepInvalidoException e){
+            System.out.println("Numero de cep invalido");
+
+        }
+
+    }   
+    
+    static String formatarCep (String cep) throws CepInvalidoException{
+        if(cep.length()!= 8)
+            throw new CepInvalidoException();
+        return "23.765.064";    
+    }
+
+}
+
+
